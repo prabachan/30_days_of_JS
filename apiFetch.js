815d95364c196df8ca4bcb3ba51eaec435d59cf1
Prabachan <bprabachan@gmail.com>
2023-07-27 17:31:30 +0545

    apiFetch

---
+++ apiFetch.js
@@ -0,0 +1,7 @@
+const url = 'https://restcountries.com/v2/all'
+fetch(url)
+  .then(response => response.json())
+  .then(data => {
+    console.log(data)
+  })
+  .catch(error => console.error(error))
