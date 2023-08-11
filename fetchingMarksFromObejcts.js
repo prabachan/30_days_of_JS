aef422618801fd1c483040ec1a97221dee8a6eef
Prabachan <bprabachan@gmail.com>
2023-08-11 13:33:21 +0545

    array and object

--- fetchingMarksFromObejcts.js
+++ fetchingMarksFromObejcts.js
@@ -6,7 +6,14 @@ rishav:32,
 dost:80
 }
 
-for(let i=0; i<=Object.keys(marks).length;i++)
-{
-    console.log('The marks of '+ Object.keys(marks)[i]+ 'is' + marks[Object.keys(marks)[i]])
+// //traditional method
+// for(let i=0; i<=Object.keys(marks).length;i++)
+// {
+//     console.log('The marks of '+ Object.keys(marks)[i] + ' is ' + marks[Object.keys(marks)[i]])
+// }
+
+//easy way to do it
+
+for(let key in marks){
+    console.log('the marks of' + key 'is' + key[marks])
 }
